ebd899538603243716c74309dbee955bbd5d46db
gbonis <gbonis@debian.gbonis>
2025-06-25 14:35:17 +0200

    tried something git clean

--- bash/scripts/gits_clean_check_2.sh
+++ bash/scripts/gits_clean_check_2.sh
@@ -3,7 +3,7 @@
 i=1
 var=0
 
-if [ "$(env | grep '^LANG')" = "LANG=fr_FR.UTF-8" ]			## j'avais un probleme parce que j'avais un commentaire avant, avec cette meme ligne commentée
+if [ "$(env | grep '^LANG')" = "LANG=fr_FR.UTF-8" ]			## j'avais un probleme parce que j'avais un commentaire sur la ligne préc, avec cette meme ligne commentée, pq ?
 then
 	export LANG=en_US
 fi
@@ -17,7 +17,7 @@ while [ true ]
 			exit
 		fi
 		cd $var
-		if [[ "$(git status | sed -n 4p)" = "nothing to commit, working tree clean"  &&  "$(git status | sed -n 2p)" = "Your branch is up to date with 'origin/master'."  || "$(git status | sed -n 2p)" = "Your branch is up to date with 'origin/main'." ]]  ## fail if main and note master  ## aussi pourquoi il faut tj des spaces, et des doubles [[
+		if [[ "$(git status | sed -n 4p)" = "nothing to commit, working tree clean"  &&  "$(git status | sed -n 2p)" = "Your branch is up to date with 'origin/master'."  || "$(git status | sed -n 2p)" = "Your branch is up to date with 'origin/main'." ]]  ## aussi pourquoi il faut tj des spaces, et des doubles [[ 
 		then
 			cd - 1>/dev/null
 			echo " SUCCESS : $var "
@@ -31,7 +31,11 @@ while [ true ]
 exit
 
 ## TODO
-## attention a la maison j'ai des fatal not a git repo
-## fail if french
+## attention a la maison j'ai des fatal not a git repo (avec la commande find qui ne devrait trouver que des .git
 
 ## faire un script aussi pour pull quand j'arrive au début de la journée. Voir meme un script qui prends les deux possibilitées.
+
+
+## LOG :
+## if origin/main instead of origin/master, failed the tests
+## system can be in another language and can fail the tests
