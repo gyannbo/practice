3e235d58cf4689bcbf8ee9f2c6f4aee8d534e6ba
gbonis <gbonis@debian.gbonis>
2025-07-15 16:56:39 +0200

    started on collections

---
+++ python/concepts/collections.py
@@ -0,0 +1,20 @@
+#collection = {'un': 'deux', 'deux': 'trois', 'trois': 'quatre'}
+#
+#for left, right in collection.items:
+#	print(collection[left])
+
+
+# Create a sample collection
+users = {'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
+
+# Strategy:  Iterate over a copy
+for user, status in users.copy().items():
+    if status == 'inactive':
+        del users[user]
+
+# Strategy:  Create a new collection
+active_users = {}
+for user, status in users.items():
+    if status == 'active':
+        active_users[user] = status
+        print(active_users)
