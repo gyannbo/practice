ab3a809b45d593dd6b585059dc1a102486d0c9c1
gbonis <gbonis@debian.gbonis>
2025-07-29 15:03:30 +0200

    correc mathMultiple

--- python/concepts/argv.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-if len(sys.argv) != 4:		# : sont nécessaires 
-	print("usage : python file.py arg1 arg2 arg3")
-	exit()
-print(sys.argv[0])
-print(sys.argv[1])
-print(sys.argv[2])
-print(sys.argv[3])
--- python/concepts/forloop.py
+++
@@ -1,6 +0,0 @@
-wordlist = ["un", "deux", "trois", "quatre"]
-
-for a in wordlist:
-	print(a, len(a))
-exit()
-
