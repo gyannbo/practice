9a5ca0407deac780676cae3cc8162d661096f1f0
gbonis <gbonis@debian.gbonis>
2025-07-29 14:12:42 +0200

    work on match and classes

--- python/concepts/matchMultiple.py
+++ python/concepts/matchMultiple.py
@@ -6,3 +6,40 @@ for x in list:
 			print("success")
 		case _:
 			print("fail")
+
+##########
+
+point = (8, 67)
+
+match point:
+	case (9, x):
+		print(x)
+	case (x, 67):
+		print(x)
+
+del point 
+
+##########
+
+class SomePoint:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
+point = SomePoint(3, 3)
+point2 = SomePoint(27, 27)
+
+def where_is(point, point2):
+	match point:
+		case SomePoint(y = 3, x = 3) as p:   ### içi nom de la classe, pas le nom de la variable
+			print(f"Success : x={p.x} y={p.y}")
+		case _:
+			print(f"fail")
+	match point2:
+		case SomePoint(x = 3, y = 3) as p:   ### içi nom de la classe, pas le nom de la variable
+			print(f"Success : x={p.x} y={p.y}")
+		case _:
+			print("fail")
+
+where_is(point, point2)
+
