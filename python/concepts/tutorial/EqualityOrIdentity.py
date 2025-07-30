a3dfa62f2338dfd82c181d40336eabab985e3f0e
gbonis <gyann.bonis@gmail.com>
2025-07-31 00:41:29 +0200

    equality and indentity python

---
+++ python/concepts/tutorial/EqualityOrIdentity.py
@@ -0,0 +1,26 @@
+a = 10
+b = 10
+c = 30
+
+print('by equality')
+print(a == b)  ## comparison by equality (vallue of the object)
+print(a == c)
+print()
+
+a = True
+b = True
+c = False
+
+print('by indentity')
+print(a is True)   ## comparison by indentity, is it the same object 
+print(a is False)
+print()
+
+list1 = [1, 2, 3]
+list2 = [1, 2, 3]
+list3 = list2
+
+
+print(list1 == list2)
+print(list1 is list2)
+print(list3 is list2)
