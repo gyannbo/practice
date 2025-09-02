516e5ca8268589c632ffdf81788dcb8ff2cc6b4d
Guillaume Bonis <gyann.bonis@gmail.com>
2025-09-02 16:20:16 +0200

    deux tp script

---
+++ bash/tpeni/tp5.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+vert='\033[5;32m'
+rouge='\033[3;31m'
+nc='\033[0m'
+user=$1
+
+if [[ -z $user ]]
+	then
+		read -p "Veuillez entrer un utilisateur: " user
+		echo $user
+		if [[ -z $user ]]
+			then
+				echo 'Erreur de saisie.             Enculé'
+				exit 2
+			fi
+	fi
+
+id $user > /dev/null 2>&1
+if [[ $? -eq 0 ]]
+	then
+		echo -e "$vert l'utilisateur existe $nc"
+	else
+		echo -e "$rouge l'utilisateur n'existe pas"
+	fi
+
---
+++ bash/tpeni/tp6.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+echo -en "Que voulez vous faire: \n \
+	Information sur le matériel (matos)\n \
+	Information sur l'espace (espace)\n \
+	Information sur les paquets (paquet)\n \
+	Information sur les répertoires (rep)\n \
+	Quitter (quit)"	
+
+read -p " : " choix trash
+
+case ${choix,,} in
+	matos|m|M)
+		read -p "CPU ou PCI ?: " matos trash
+		if [[ ${matos,,} == "cpu" ]]
+			then
+				lscpu	
+			elif [[ ${matos,,} == "pci" ]]
+			then
+				lspci	
+			fi
+	;;
+
+	espace|e|E)
+		read -p "Mo ou inode ?: " info trash
+		if [[ ${info,,} == "mo" ]]
+			then
+				df -h	
+		elif [[ ${info,,} == "inode" ]]
+			then
+				df -i	
+			fi
+	;;
+
+	paquet|p|P)
+		dpkg --get-selections
+	;;
+
+	rep|r|R)
+		du -hs /*
+	;;
+
+	quit|q|Q)
+	;;
+
+	*)
+		echo "syntac error"
+		exit 2
+	;;
+esac	
+	
+sleep 2
+echo fin
+	
+	
